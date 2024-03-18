7177b752f5f83cc42d922c37f869f1d27a872b95
ROYFORDMUGENDI <mugendiroyford1@gmail.com>
2024-03-18 15:32:59 +0300

    wk2

---
+++ list.py
@@ -0,0 +1,10 @@
+my_list=[]
+my_list.extend([10,20,30,40,50])
+my_list.insert(1,15)
+my_list.extend([60,70,80])
+my_list.pop()
+my_list.sort()
+my_list.index(30)
+index=my_list.index(30)
+
+print(index)
